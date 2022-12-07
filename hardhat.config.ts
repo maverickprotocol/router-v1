7ce0cc9303d3a49749f872d02e5f7da68e713d65
Trung Dinh <trqdinh@gmail.com>
2022-12-07 07:06:26 -0800

    2ea310b28fd52cf45310960530c7b0cca0daff84

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -24,7 +24,7 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 20
+            runs: 200
           },
         }
       }
